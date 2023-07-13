b26a6eb5e0149532949ab2e6ed2c6b73bf4ea924
BuzzAdonis <55252048+BuzzAdonis@users.noreply.github.com>
2023-07-13 14:14:30 -0400

    Primer commit

---
+++ Mvc_Curso_BootsTrap_06_07_2033/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Mvc_Curso_BootsTrap_06_07_2033.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
